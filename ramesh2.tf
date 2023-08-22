6dc5f8b6f7e1e72b957bf85889b8be50603a1af8
cr-github1 <139527399+cr-github1@users.noreply.github.com>
2023-08-22 20:44:45 +0530

    Create ramesh2.tf

---
+++ ramesh2.tf
@@ -0,0 +1,15 @@
+variable "filename" {
+    default = "nc.txt"
+    type = string 
+}
+variable "content" {
+    default = "devops aws course" 
+}
+variable "prefix" {
+    default = "A"
+    type = string
+    }
+
+    variable "length" {
+        default = "1"
+        type = number
