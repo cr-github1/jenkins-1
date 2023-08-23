17cadb489e1e1e1581fd13e3d514123b096bed2c
cr-github1 <139527399+cr-github1@users.noreply.github.com>
2023-08-23 17:54:03 +0530

    Update terraform 3.tf

--- terraform 3.tf
+++ terraform 3.tf
@@ -1,17 +1,10 @@
-variable "filename" {
-    default = "myc1vr.txt"
-    type = string 
-  }
-variable "content" {
-    default = "this is ramesh"
-  }
-variable "prefix" {
-    default = "MR"
-      }
-      variable "separator" {
-        default = "."
-        
-      }
-variable "length" {
-    default = "1"
-  }
+resource "local_file" "c1" {
+  filename = var.filename
+  content = var.content
+
+}
+resource "random_pet" "c2" {
+    prefix = "MR"
+    separator = "."
+    length = "1"
+}
