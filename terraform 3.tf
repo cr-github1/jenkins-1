2302af4c8c54418364c4870a9d8b4715795767bb
cr-github1 <139527399+cr-github1@users.noreply.github.com>
2023-08-23 17:46:45 +0530

    Create terraform 3.tf

---
+++ terraform 3.tf
@@ -0,0 +1,17 @@
+variable "filename" {
+    default = "myc1vr.txt"
+    type = string 
+  }
+variable "content" {
+    default = "this is ramesh"
+  }
+variable "prefix" {
+    default = "MR"
+      }
+      variable "separator" {
+        default = "."
+        
+      }
+variable "length" {
+    default = "1"
+  }
