9c03681d03ca3d0c0f338065f18a9ed1def7712d
cr-github1 <139527399+cr-github1@users.noreply.github.com>
2023-08-26 13:54:17 +0530

    Update terraform4.tf

--- terraform4.tf
+++ terraform4.tf
@@ -1,12 +1,4 @@
-resource "local_file" "foreach1" {
-    filename = each.value
-    content = "this is for each topic"
-    for_each = toset(var.filename)
-
-}
-variable "filename" {
-default = [
-    "a.txt",
-    "b.txt"
-]  
+resource "local_file" "test" {
+  filename = "ramesh2.txt"
+  content = "this is terra1"
 }
